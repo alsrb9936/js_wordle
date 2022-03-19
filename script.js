2ffefc1c32c3bdcdbde0d9b3ecf3e72fda18a64e
Mingyu <ghfn123@gmail.com>
2022-03-19 12:17:02 +0900

    create simple worlde game

---
+++ script.js
@@ -0,0 +1,34 @@
+var 답안 = "abcde";
+
+document.querySelector("button").addEventListener("click", function () {
+  var input = document.querySelectorAll(".input");
+
+  // 문자가 자리도 맞을 때
+  for (let i = 0; i < 5; i++) {
+    // 문자가 자리도 맞을 때
+    if (input[i].value == 답안[i]) {
+      input[i].style.backgroundColor = "lightgreen";
+    }
+    // 문자가 포함만 되어 있을 때
+    else if (답안.includes(input[i].value)) {
+      input[i].style.backgroundColor = "lightyellow";
+    }
+
+    // 아무것도 안 맞을 때
+    else {
+      input[i].style.backgroundColor = "lightgray";
+    }
+
+    input[i].classList.remove("input");
+  }
+
+  var template = `<div>
+        <input class="input" maxlength="1"/>
+        <input class="input" maxlength="1"/>
+        <input class="input" maxlength="1"/>
+        <input class="input" maxlength="1"/>
+        <input class="input" maxlength="1"/>
+        </div>`;
+
+  document.querySelector("body").insertAdjacentHTML("beforeend", template);
+});
